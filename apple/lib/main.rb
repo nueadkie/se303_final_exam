f7384c32b8bb802dbc78d953a183ae6fd1d31f71
Chris Miraflor <chris.j.miraflor@gmail.com>
2022-06-07 10:56:15 -0700

    Moved invocation of new function such that its result is now used in place of old code

--- apple/lib/main.rb
+++ apple/lib/main.rb
@@ -3,13 +3,13 @@
 
 def story
   first = "Mr. Dursley was the director of a firm called Grunnings, which made drills."
-  middle = newlines
   middle = ""
   middle << "\n"
   middle << "\n"
   middle << "\n"
   middle << "\n"
   middle << "\n"
+  middle = newlines
   conclusion = "And that was it."
   first + middle + conclusion
 end
