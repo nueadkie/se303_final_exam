81f92aaeb3db0666c59f0c7306b51e5da8f9d8c5
Chris Miraflor <chris.j.miraflor@gmail.com>
2022-06-07 11:24:26 -0700

    Delete unused

--- cherry/lib/main.rb
+++ cherry/lib/main.rb
@@ -6,13 +6,6 @@ def total_and_smallest
   data = [23, 15, 8, 16, 4, 42]
   smallest = data.first
   total = total(data)
-  total = 0
-  data.each do |d|
-    total += d
-    if d < smallest
-      smallest = d
-    end
-  end
   [total, smallest(data)]
 end
 
